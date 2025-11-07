5a09dd0d1b6c2133de56894c7e65314c8f4253a7
nitinrandhawan <nitinrandhawan@gmail.com>
2025-11-07 17:57:28 +0530

    add swiper dependency in package.json and pnpm-lock.yaml; enhance Home page layout by integrating TopCollegesSection and RankingsSection, while updating heroSlider images for improved visuals.

--- src/app/pages/Home/heroSlider.jsx
+++ src/app/pages/Home/heroSlider.jsx
@@ -8,11 +8,11 @@ import { cn } from "@/lib/utils";
 const desktopSlides = [
   // { bgImage: "/images/117hbanner_placement-piyush.webp" },
   // { bgImage: "/images/101hbanner_offcampus-banner.webp" },
-  { bgImage: "/images/banner_Image.webp" },
+  { bgImage: "/images/bg-1.webp" },
 ];
 
 const mobileSlides = [
-  { bgImage: "https://noida.gla.ac.in/banner/banner_image_res.webp" },
+  { bgImage: "/images/bg-1-mobile.webp" },
   // { bgImage: "https://www.gla.ac.in/Uploads/banner/118hbanner_placement-piyush-mob.webp" },
 ];
 
